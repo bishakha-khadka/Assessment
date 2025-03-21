0935fecaea9c481a518f824281ef7f14dc92daec
Bishakha Khadka <156450498+bishakha-khadka@users.noreply.github.com>
2025-03-21 16:31:24 +0545

    create count_inversion.js

---
+++ count_inversion.js
@@ -0,0 +1,15 @@
+function count_inversion(arr) {
+    let count=0;
+    let length=arr.length;
+    for(let i=0;i<length;i++){
+        for (let j=i+1; j<length; j++) {
+            if (arr[i]>arr[j] && i<j){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+const arr = [8,4,2,1];
+console.log(count_inversion(arr));
